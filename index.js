6d8f777af26b721d7e98120ecd04ae1f4db49842
José Ramírez <jwh743@gmail.com>
2025-02-27 08:52:54 -0300

    Fix del formato unix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/api/:date_string", (req, res) => {
 
   if (/\d{5,}/.test(dateString)) {
     let dateInt = parseInt(dateString);
-    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
   } else {
     let dateObject = new Date(dateString);
 
